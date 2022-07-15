cb94bedb25e5a108db10266cc02b37d308fbabb1
shubhangitayade2 <105914219+shubhangitayade2@users.noreply.github.com>
2022-07-15 12:32:37 +0530

    Add files via upload

---
+++ LoginController.cs
@@ -0,0 +1,55 @@
+﻿using EcommerceWebApi.Interfaces;
+using EcommerceWebApi.Models;
+using EcommerceWebApi.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        EcommerceDBContext db;
+        IJWTMangerRepository iJWTMangerRepository;
+        public LoginController(EcommerceDBContext _db, IJWTMangerRepository _iJWTMangerRepository)
+        {
+            db = _db;
+            iJWTMangerRepository = _iJWTMangerRepository;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            var token = iJWTMangerRepository.Authenicate(loginViewModel, false);
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(token);
+        }
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            LoginViewModel login = new LoginViewModel();
+            login.UserName = registerViewModel.UserName;
+            login.Password = registerViewModel.Password;
+            var token = iJWTMangerRepository.Authenicate(login, true);
+            if (token.IsUserExits)
+            {
+                return Ok("User already exists");
+            }
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(token);
+        }
+    }
+}
---
+++ TblPayment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EcommerceWebApi.Models
+{
+    public partial class TblPayment
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? CardNumber { get; set; }
+        public string StreedAddress { get; set; }
+        public string City { get; set; }
+        public int? ZipCode { get; set; }
+        public string Email { get; set; }
+    }
+}
